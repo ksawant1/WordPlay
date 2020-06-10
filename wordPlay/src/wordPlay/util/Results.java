df5001f2f3db395e05e9171d1bab4674019e781d
ksawant1@binghamton.edu <ksawant1@binghamton.edu>
2020-06-10 03:03:52 -0400

    created an array list data structure to store rotated final result

--- wordPlay/src/wordPlay/util/Results.java
+++ wordPlay/src/wordPlay/util/Results.java
@@ -1,5 +1,33 @@
 package wordPlay.util;
-
+import java.util.ArrayList;
 public class Results implements FileDisplayInterface, StdoutDisplayInterface {
-	
+    ArrayList<String> words= new ArrayList<String>();
+
+    public Results(){
+        
+        
+
+    }	
+    public void writeToFile(){
+        
+
+            for (int i=0;i<words.size();i++){
+                System.out.println(words.get(i));
+            }
+    }
+
+    public void writeToStdout(){
+
+    }
+
+    public void store(String word){
+        words.add(word);
+        //writeToFile();
+        System.out.println(words);
+
+        
+        
+    }
+    
+
 }
