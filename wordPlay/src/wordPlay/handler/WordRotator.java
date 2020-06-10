e26abf811fc77bb929f6dec39bed1429a081996e
ksawant1@binghamton.edu <ksawant1@binghamton.edu>
2020-06-10 02:59:48 -0400

    created instance or result and passed the rotated words

--- wordPlay/src/wordPlay/handler/WordRotator.java
+++ wordPlay/src/wordPlay/handler/WordRotator.java
@@ -1,31 +1,36 @@
 package wordPlay.handler;
-
+import  wordPlay.util.Results;
 public final class WordRotator {
     private String inputword;
+     String resultant_word;
     String ans;
+Results resultantword=new Results();
+    public void Wordr(String newword,int index){
+        
+             if(newword.contains(".")){
+                  String lastword=newword.substring(0,newword.length()-1);
+                  resultant_word=rotate(lastword,index);
+                  resultant_word=resultant_word.toString()+".";
+                 
+                  resultantword.store(resultant_word);
 
-    public WordRotator(String newword,int index){
-             
-             rotate(newword,index);
-         
+             }
+             else{
+                 resultant_word=rotate(newword,index);
+                 resultantword.store(resultant_word);
 
-         
-
-
+             }
 
     }
 
-    public void rotate(String newword,int index){
-        //System.out.println(newword.length()-index);
+    public String rotate(String newword,int index){
         String ans= newword.substring(newword.length()-index)+newword.substring(0,newword.length()-index);
-        System.out.println(ans);
+        return ans;
         
 
-         
+    
 
     }
-               
-
 
 	
 }
